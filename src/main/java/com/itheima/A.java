9d9db41e5c1e5597718298fa121c8f1f0ce45054
sjh <1097772818@qq.com>
2019-06-08 19:11:12 +0800

    11111

---
+++ src/main/java/com/itheima/A.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+/**
+ * @Author: SongJunHui
+ * email:1097772818@qq.com
+ * blog: https://blog.csdn.net/s4742488
+ * @Date: 2019/6/8 18:55
+ */
+public class A {
+
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaa");
+    }
+
+}
